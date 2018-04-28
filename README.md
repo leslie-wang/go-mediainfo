5f9351f60c5308666266db478026a99dfaa1e1c1
Qi Wang <qiwa@Qis-MacBook-Pro.local>
2018-04-28 15:52:09 -0700

    fix import filepath for example, also add timestamp info in readme
    Signed-off-by: Qi Wang <qiwa@Qis-MacBook-Pro.local>

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Basic MediaInfo bindings for Go.
 ## Installing
 
 go get github.com/dwbuiten/go-mediainfo/mediainfo
+
+## To generate timestamp during ffmpeg transcoding
+ffmpeg -i <input> -metadata date="UTC 2013-08-08 08:08:08" <output>
--- example/example.go
+++ example/example.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "mediainfo"
+	"flag"
+	"fmt"
+
+	"github.com/leslie-wang/go-mediainfo/mediainfo"
 )
 
 func main() {
 
-    flag.Parse()
-    args := flag.Args()
-    if len(args) < 2 {
-        fmt.Println("Not enough arguments.")
-        return
-    }
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Println("Not enough arguments.")
+		return
+	}
 
-    /* Load the shared library. */
-    mediainfo.Init()
+	/* Load the shared library. */
+	mediainfo.Init()
 
-    /* Open and parse the file. */
-    info, err := mediainfo.Open(args[0])
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer info.Close()
+	/* Open and parse the file. */
+	info, err := mediainfo.Open(args[0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer info.Close()
 
-    /* Get the info. */
-    val, err := info.Get(args[1], 0, mediainfo.Video)
-    if err != nil {
-        fmt.Println(err);
-        return
-    }
+	/* Get the info. */
+	val, err := info.Get(args[1], 0, mediainfo.General)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    fmt.Println(val)
+	fmt.Println(val)
 }
